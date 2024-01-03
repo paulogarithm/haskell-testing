9936727ef3c7ec695b4a4b9dc5c711956fd9a548
Paul Parisot <paul.parisot@epitech.eu>
2024-01-03 19:29:08 +0100

    Add: stuff

--- init.sh
+++ init.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Get the tests directory
-__basedir=$(dirname "$0")
-__here="$PWD/$__basedir"
+__here=$(dirname "$0")
 
 # Move/Remove useless stuff
 mv "$__here/LICENSE" "$__here/README.md" tests
@@ -13,9 +12,10 @@ __packagemanager=$(which apt || which dnf || which brew)
 sudo $__packagemanager install ghc
 
 # Update & Move test in your own test stuff
-mv "$__here/.gitignore" "$__here/__tests"
-mv "$__here/__tests" "$__here/.."
 cabal update
+mv "$__here" "foo_$__here"
+__here=$(dirname "$0")
+mv "$__here/tests" "$__here/.."
 
 # Remove this folder
 rm -rf "$__here"
